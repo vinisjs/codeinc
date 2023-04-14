c02e053048eb84e4506d5f6a89ebe60a3c0ddc48
Vinicius Souza <contato@viniccius.com.br>
2023-04-14 14:55:21 -0400

    if

--- biblioteca/index.c
+++ biblioteca/index.c
@@ -1,7 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+typedef struct
+{
+  char name[30];
+  int qnt;
+} Book;
+
+typedef struct
+{
+  Book books;
+  int qnt;
+  int date;
+} Library;
+
+typedef struct
+{
+  char name[30];
+  int age;
+  char email[100];
+} Person;
+
+void createUser(Person *US, Library *Biblioteca, Book *Livro);
+void createBook(Person *USC, Library *Biblioteca, Book *Livro);
+void createLibrary(Person *USC, Library *Biblioteca, Book *Livro);
+void menu(Person *USC, Library *Biblioteca, Book *Livro);
+
+void impress();
 
 int main()
 {
-  printf("\n Hello World");
+
+  Person Usuario;
+  Library Biblioteca;
+  Book Livros;
+
+  system("cls");
+  setlocale(LC_ALL, "Portuguese");
+  printf("\nSISTEMA DE GERÊNCIAMENTO DE LEITURA\n\n");
+  createUser(&Usuario, &Biblioteca, &Livros);
+}
+
+void createUser(Person *US, Library *Biblioteca, Book *Livro)
+{
+  printf("### Criação de Usuário ###\n\n");
+
+  printf("Digite seu nome: ");
+  scanf(" %[^\n]s", (*US).name);
+
+  printf("Digite sua idade: ");
+  scanf(" %d", &(*US).age);
+
+  printf("Digite seu e-mail: ");
+  scanf(" %[^\n]s", (*US).email);
+
+  printf("Sucesso\n\n");
+  menu(US, Biblioteca, Livro);
+}
+
+void menu(Person *USC, Library *Biblioteca, Book *Livro)
+{
+  int op;
+  do
+  {
+    printf("## Menu ## \n\n ");
+    printf("1 - Criar Registro\n ");
+    printf("0 - Sair\n");
+    scanf(" %d", &op);
+
+    if (op == 1)
+      createLibrary(USC, Biblioteca, Livro);
+    else
+      printf("Invalid Option, try again\n");
+  } while (op != 0);
+}
+
+void createLibrary(Person *USC, Library *Biblioteca, Book *Livro)
+{
+  int t;
+  printf("Cadastro Biblioteca\n\n");
+
+  printf("Quantos dias de leitura: ");
+  scanf(" %d", &(*Biblioteca).qnt);
+
+  t = (*Biblioteca).qnt;
+
+  printf("tempo: %d", t);
 }
